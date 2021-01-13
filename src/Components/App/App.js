e51950d940c982db1761b7597c6f3c3f19278cc1
Aaron Townsend <atownse17@gmail.com>
2021-01-13 17:50:44 -0600

    Build component for users to create characters and route to app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Home from '../Home/Home'
+import CharacterCreator from '../Character-creator/Character-Creator'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <main>
-      <Route exact path="/" component={Home} />
+      <Switch>
+        <Route path="/character-creator" component={CharacterCreator} />
+        <Route exact path="/" component={Home} />
+      </Switch>
     </main>
   );
 }
---
+++ src/Components/Character-creator/Character-Creator.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import './Character-Creator.css'
+
+export default function CharacterCreator() {
+  return (
+    <div className="complete-page">
+      <h1>Make a Character</h1>
+      <form>
+        <div className="name-input">
+          <input
+            type="text"
+            name="name"
+            placeholder="Character Name Here"
+            // value={this.state.name}
+            // onChange={(e) => {this.handleChange}}
+          />
+        </div>
+        <div className="container">
+          <div className="character-creation">
+            <h2>Options</h2>
+            <label for="birthplace">Birthplace</label>
+            <label for="residence">Current City</label>
+            <label for="job">Job</label>
+            <label for="height">Height</label>
+            <label for="eye-color">Eye Color</label>
+            <label for="music">Favorite Music Genre</label>
+            <label for="quote">Favorite Quote</label>
+            <label for="insult">Favorite Insult</label>
+          </div>
+          <div className="character-creation" id="yes">
+            <h2>Yes</h2>
+            <input type="radio" id="birthplace" name="birthplace" />
+            <input type="radio" id="residence" name="residence" />
+            <input type="radio" id="job" name="job" />
+            <input type="radio" id="height" name="height" />
+            <input type="radio" id="eye-color" name="eye-color" />
+            <input type="radio" id="music" name="music" />
+            <input type="radio" id="quote" name="quote" />
+            <input type="radio" id="insult" name="insult" />
+          </div>
+          <div className="character-creation" id="no">
+            <h2>No</h2>
+            <input type="radio" id="birthplace" name="birthplace" />
+            <input type="radio" id="residence" name="residence" />
+            <input type="radio" id="job" name="job" />
+            <input type="radio" id="height" name="height" />
+            <input type="radio" id="eye-color" name="eye-color" />
+            <input type="radio" id="music" name="music" />
+            <input type="radio" id="quote" name="quote" />
+            <input type="radio" id="insult" name="insult" />
+        </div>
+        </div>
+      </form>
+    </div>
+  )
+}
