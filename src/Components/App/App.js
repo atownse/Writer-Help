9e5433fe4bd3423fc601574e4cabbefe1ef64daf
Aaron Townsend <atownse17@gmail.com>
2021-01-13 20:00:47 -0600

    Start basics for archive component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Home from '../Home/Home'
 import CharacterCreator from '../Character-creator/Character-Creator'
+import Archive from '../Archive/Archive'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <main>
       <Switch>
-        <Route path="/character-creator" component={CharacterCreator} />
-        <Route exact path="/" component={Home} />
+        <Route path="/character-creator" component={ CharacterCreator } />
+        <Route path="/archive" component={ Archive } />
+        <Route exact path="/" component={ Home } />
       </Switch>
     </main>
   );
---
+++ src/Components/Archive/Archive.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Archive extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {  
+    return (
+      <div>
+        <h1>Saved Characters</h1>
+      Character Cards Here
+      </div>
+    )
+  }
+}
+
+export default Archive
