fcb80e013b6cbed6a965e0cbc1de3db9733e917e
Aaron Townsend <atownse17@gmail.com>
2021-01-17 11:37:01 -0600

    Fix for warning in Character Creator

--- src/Components/Character-creator/Character-Creator.js
+++ src/Components/Character-creator/Character-Creator.js
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom'
 export default function CharacterCreator(props) {
   const [name, setName] = useState('')
   const [birthplace, setBirthplace] = useState(false)
-  const [residence, setResidence] = useState()
-  const [job, setJob] = useState('')
-  const [height, setHeight] = useState('')
-  const [eyeColor, setEyeColor] = useState('')
+  const [residence, setResidence] = useState(false)
+  const [job, setJob] = useState(false)
+  const [height, setHeight] = useState(false)
+  const [eyeColor, setEyeColor] = useState(false)
   // const [music, setMusic] = useState('')
   // const [quote, setQuote] = useState('')
   // const [insult, setInsult] = useState('')
@@ -28,35 +28,35 @@ export default function CharacterCreator(props) {
         </div>
         <div className="container">
           <div className="character-creation">
-            <label for="birthplace">Birthplace</label>
+            <label htmlFor="birthplace">Birthplace</label>
             <input type="checkbox" name="birthplace" checked={ birthplace } onChange={ (e) => setBirthplace(!birthplace) } />
           </div>
           <div>
-            <label for="residence">Current City</label>
+            <label htmlFor="residence">Current City</label>
             <input type="checkbox" name="residence" checked={ residence } onChange={ (e) => setResidence(!residence) } />
           </div>
           <div>
-            <label for="job">Job</label>
+            <label htmlFor="job">Job</label>
             <input type="checkbox" name="job" checked={ job } onChange={ (e) => setJob(!job) } />
           </div>
           <div>
-            <label for="height">Height</label>
+            <label htmlFor="height">Height</label>
             <input type="checkbox" name="height" checked={ height } onChange={ (e) => setHeight(!height) } />
           </div>
           <div>
-            <label for="eyeColor">Eye Color</label>
+            <label htmlFor="eyeColor">Eye Color</label>
             <input type="checkbox" name="eyeColor" checked={ eyeColor } onChange={ (e) => setEyeColor(!eyeColor) } />
           </div>
           {/* <div>
-            <label for="music">Favorite Music</label>
+            <label htmlFor="music">Favorite Music</label>
             <input type="checkbox" name="music" checked={ music } onChange={ (e) => setMusic(!music) } />
           </div>
           <div>
-            <label for="quote">Favorite Quote</label>
+            <label htmlFor="quote">Favorite Quote</label>
             <input type="checkbox" name="quote" checked={ quote } onChange={ (e) => setQuote(!quote) } />
           </div>
           <div>
-            <label for="insult">Favorite Insult</label>
+            <label htmlFor="insult">Favorite Insult</label>
             <input type="checkbox" name="insult" checked={ insult } onChange={ (e) => setInsult(!insult) } />
           </div> */}
         </div>
