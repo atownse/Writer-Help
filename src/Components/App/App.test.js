cfd68416f23461edd9270aff2f413a28099606be
Aaron Townsend <atownse17@gmail.com>
2021-01-17 11:36:27 -0600

    Test routes in App

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,8 +1,42 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { screen, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import "@testing-library/jest-dom"
+import App from "../App/App"
+import { MemoryRouter } from "react-router-dom"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('should route to character creator correctly', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    )
+    
+    // const homeBtn = screen.getByText('Home')
+    // const header = screen.getByText('Saved Characters')
+    const newCharacterBtn = screen.getByText('Create a Character')
+    userEvent.click(newCharacterBtn)
+    expect(screen.getByText('Make a Character')).toBeInTheDocument()
+
+    // expect(homeBtn).toBeInTheDocument()
+    // expect(header).toBeInTheDocument()
+    // expect(newCharacterBtn).toBeInTheDocument()
+  }),
+
+  it('should route to character creator correctly', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    )
+    
+    // const homeBtn = screen.getByText('Home')
+    // const header = screen.getByText('Saved Characters')
+    const archiveBtn = screen.getByText('View Characters')
+    userEvent.click(archiveBtn)
+    expect(screen.getByText('Saved Characters')).toBeInTheDocument()
+    // expect(homeBtn).toBeInTheDocument()
+    // expect(header).toBeInTheDocument()
+    // expect(newCharacterBtn).toBeInTheDocument()
+  })
+})
