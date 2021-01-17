8b6e3a488ca1c73a6e9fc23e477747ca40e061df
Aaron Townsend <atownse17@gmail.com>
2021-01-17 12:32:42 -0600

    Clean up App tests

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -11,32 +11,21 @@ describe('App', () => {
         <App />
       </MemoryRouter>
     )
-    
-    // const homeBtn = screen.getByText('Home')
-    // const header = screen.getByText('Saved Characters')
+
     const newCharacterBtn = screen.getByText('Create a Character')
     userEvent.click(newCharacterBtn)
     expect(screen.getByText('Make a Character')).toBeInTheDocument()
-
-    // expect(homeBtn).toBeInTheDocument()
-    // expect(header).toBeInTheDocument()
-    // expect(newCharacterBtn).toBeInTheDocument()
   }),
 
-  it('should route to character creator correctly', () => {
+  it('should route to archive correctly', () => {
     render(
       <MemoryRouter>
         <App />
       </MemoryRouter>
     )
     
-    // const homeBtn = screen.getByText('Home')
-    // const header = screen.getByText('Saved Characters')
     const archiveBtn = screen.getByText('View Characters')
     userEvent.click(archiveBtn)
     expect(screen.getByText('Saved Characters')).toBeInTheDocument()
-    // expect(homeBtn).toBeInTheDocument()
-    // expect(header).toBeInTheDocument()
-    // expect(newCharacterBtn).toBeInTheDocument()
   })
 })
