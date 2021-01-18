f150853fb15209f0c98d1fc831b6d7452204f22f
Aaron Townsend <atownse17@gmail.com>
2021-01-18 12:31:14 -0600

    Add proptypes to Card component

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Card.css'
+import PropTypes from 'prop-types'
 
 export default function Card({id, name, birthplace, job, residence, height, eyeColor}) {
   return (
@@ -8,8 +9,18 @@ export default function Card({id, name, birthplace, job, residence, height, eyeC
         <p>Profession: { job || 'Unknown' }</p>
         <p>Birthplace: { birthplace || 'Unknown' }</p>
         <p>Home: { residence || 'Unknown' }</p>
-        <p>Height: { height || 'Unknown' }</p>
+        <p>Height: { height || 'Unknown' } meters</p>
         <p>Eye Color: { eyeColor || 'Unknown' }</p>
       </section>
   )
+}
+
+Card.propTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  birthplace: PropTypes.string,
+  job: PropTypes.string,
+  residence: PropTypes.string,
+  height: PropTypes.string,
+  eyeColor: PropTypes.string
 }
